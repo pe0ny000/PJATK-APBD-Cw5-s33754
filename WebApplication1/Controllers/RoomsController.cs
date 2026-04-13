c9d707b38b45800ec3c2e0cc9a8fb6b46907d853
Julia Cembrowska <s33754@pjwstk.edu.pl>
2026-04-13 21:33:17 +0200

    Prepere Controllers and DTOs frameworks

---
+++ WebApplication1/Controllers/RoomsController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class RoomsControler : ControllerBase
+{
+    
+    
+}
---
+++ WebApplication1/DTOs/ReservationDto.cs
@@ -0,0 +1,6 @@
+namespace WebApplication1.DTOs;
+
+public class ReservationDto
+{
+    
+}
